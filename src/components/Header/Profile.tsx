ff6e84881b79b959baf8e50f8f17c60f0a3388dd
Tagaiatto <34107323+Tagaiatto@users.noreply.github.com>
2021-06-30 18:44:55 -0300

    Header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData: boolean;
+}
+
+export function Profile({ showProfileData }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Tagaiatto</Text>
-        <Text color="gray.300" fontSize="small">
-          edilsonmneto@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Tagaiatto</Text>
+          <Text color="gray.300" fontSize="small">
+            edilsonmneto@gmail.com
+          </Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Edilson Mendes" src="https://github.com/tagaiatto.png" />
     </Flex>
