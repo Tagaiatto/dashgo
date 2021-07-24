9de744e6e4ae6a556071828a2d768da820abd6bf
Tagaiatto <34107323+Tagaiatto@users.noreply.github.com>
2021-07-23 22:24:44 -0300

    SSR users

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -6,11 +6,12 @@ import { RiAddLine, RiPencilLine } from 'react-icons/ri';
 import { Header } from '../../components/Header';
 import { Sidebar } from '../../components/Sidebar';
 import { Pagination } from '../../components/Pagination';
-import { useUsers } from '../../services/hooks/useUsers';
+import { getUsers, useUsers } from '../../services/hooks/useUsers';
 import { queryClient } from '../../services/queryClient';
 import { api } from '../../services/api';
+import { GetServerSideProps } from 'next';
 
-export default function UserList() {
+export default function UserList({ users }) {
   const [page, setPage] = useState(1);
 
   const isWideVersion = useBreakpointValue({
@@ -18,7 +19,9 @@ export default function UserList() {
     lg: true,
   })
 
-  const { data, isLoading, isFetching, error } = useUsers(page);
+  const { data, isLoading, isFetching, error } = useUsers(page, {
+    initialData: users,
+  });
 
   async function handlePrefetchUser(userId: string) {
     await queryClient.prefetchQuery(['user', userId], async () => {
@@ -125,4 +128,14 @@ export default function UserList() {
       </Flex>
     </Box>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { users, totalCount } = await getUsers(1);
+
+  return {
+    props: {
+      users,
+    },
+  }
 }
